c1101a3f7f17622491a899a71296e4a4c73b5ad9
Devashish-Kaushik-DEV <devashishuk@outlook.com>
2025-08-20 22:14:42 +0530

    ..

--- D3-Control-flow/S3-Loops-practiceSet.py
+++ D3-Control-flow/S3-Loops-practiceSet.py
@@ -1,40 +1,52 @@
-# 1. write a program to take number from user and then print sum total value from to that n-th number
+# # 1. write a program to take number from user and then print sum total value from to that n-th number
 
-num = int(input("Enter a number: "))
-count = 0
-for i in range(1, num+1):
-    count += i
-print("Sum of 1 to n-th term is: ", count)
-
-
-# 2. write a program to print multiplication table with user input
-
-num = int(input("Enter a number: "))
-for i in range(1, 11):
-    print(f"{num} X {i} = {num * i}")
+# num = int(input("Enter a number: "))
+# count = 0
+# for i in range(1, num+1):
+#     count += i
+# print("Sum of 1 to n-th term is: ", count)
 
 
-# 3. write a program to print reverse count down from a user input 
+# # 2. write a program to print multiplication table with user input
 
-count = int(input("Enter a number: "))
-while count >= 1:
-    print(count)
-    count -= 1
-
-
-# 4. write a program to print all the even number between two user input digits
-
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-for i in range(num1, num2 + 1):
-    if i % 2 == 0:
-        print(i)
+# num = int(input("Enter a number: "))
+# for i in range(1, 11):
+#     print(f"{num} X {i} = {num * i}")
 
 
-# 5. write a program to calculate factorial of a number 
+# # 3. write a program to print reverse count down from a user input 
 
-num = int(input("Enter a number: "))
-fact = 1
-for i in range(1, num + 1):
-    fact *= i
-print("Factorial is: ", fact)
+# count = int(input("Enter a number: "))
+# while count >= 1:
+#     print(count)
+#     count -= 1
+
+
+# # 4. write a program to print all the even number between two user input digits
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# for i in range(num1, num2 + 1):
+#     if i % 2 == 0:
+#         print(i)
+
+
+# # 5. write a program to calculate factorial of a number 
+
+# num = int(input("Enter a number: "))
+# fact = 1
+# for i in range(1, num + 1):
+#     fact *= i
+# print("Factorial is: ", fact)
+
+
+# 6. write a program to guess the number 
+
+secret_number = 5
+guess = None
+while guess != secret_number:
+    guess = int(input("Enter a number to guess: "))
+    if guess == secret_number:
+        print("You guessed it right")
+    else:
+        print("wrong guess! Try again")
